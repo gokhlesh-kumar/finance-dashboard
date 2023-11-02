9a76066469a5af1cf42faed3ea60f9c1011f2e7f
Gokhlesh <gokhleshns@gmail.com>
2023-11-02 23:56:33 +0530

    Routes Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import { useMemo, useState } from 'react'
-import { themeSettings } from './theme'
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { useMemo, useState } from "react";
+import { themeSettings } from "./theme";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./screens/Navbar";
 
 function App() {
-
-  const theme = useMemo(()=>createTheme(themeSettings),[])
+  const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <>
-      <div className='app'>
-        <ThemeProvider theme={theme} >
+      <div className="app">
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" />
+            </Routes>
+          </Router>
           <CssBaseline />
         </ThemeProvider>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
