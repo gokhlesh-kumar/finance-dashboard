5d3d115ec5ed5a5727a89db5703a36fb558f958c
Gokhlesh <gokhleshns@gmail.com>
2023-11-03 00:09:33 +0530

    chngs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useMemo, useState } from "react";
 import { themeSettings } from "./theme";
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline, ThemeProvider, createTheme, Box } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./screens/Navbar";
 
@@ -10,15 +10,18 @@ function App() {
   return (
     <>
       <div className="app">
-        <ThemeProvider theme={theme}>
-          <Router>
-            <Navbar />
-            <Routes>
-              <Route path="/" />
-            </Routes>
-          </Router>
-          <CssBaseline />
-        </ThemeProvider>
+        <Router>
+          <ThemeProvider theme={theme}>
+            <Box width='100%' height='100%' p='1rem 2 rem 4rem 2rem' >
+              <Navbar />
+              <CssBaseline />
+              <Routes>
+                <Route path="/" element={<div>Home page</div>} />
+                <Route path="/prediction" element={<div>Prediction page</div>} />
+              </Routes>
+            </Box>
+          </ThemeProvider>
+        </Router>
       </div>
     </>
   );
